c6b4772d5d6ec8fcb6e60ba75f3a61dd49e77ff6
Sebastian Breuers <sebastian.breuers@elfin.de>
2022-10-08 10:45:29 +0200

    feature: graphView toggle

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -41,6 +41,7 @@ MainWindow::MainWindow(QWidget *parent)
   menu->addAction(ui->statusDock->toggleViewAction());
   menu->addAction(ui->moleculeDock->toggleViewAction());
   menu->addAction(ui->textViewDock->toggleViewAction());
+  menu->addAction(ui->graphDock->toggleViewAction());
 
   auto view = new Pipeline::View::Viewer(this);
   if (!ui->pipelineTab->layout())
