1fefed62e7de53177b49d9bbde77c97d0b8743a8
Sebastian Breuers <sebastian.breuers@elfin.de>
2022-10-15 09:43:11 +0200

    refactor: remove unused grayscale option

--- src/pipeline/view/clickableicon.cpp
+++ src/pipeline/view/clickableicon.cpp
@@ -10,12 +10,10 @@ const QSize ClickableIcon::defaultSize = QSize(42, 42);
 
 ClickableIcon::ClickableIcon(
   const QIcon& newIcon,
-  bool newGrayScale,
   QGraphicsItem* parent)
   : QObject(nullptr)
   , QGraphicsItem(parent)
   , icon(newIcon)
-  , grayScale(newGrayScale)
 {
   setAcceptHoverEvents(true);
   update();
@@ -26,10 +24,9 @@ QRectF ClickableIcon::boundingRect() const
   return QRectF(QPointF(0, 0), defaultSize);
 }
 
-void ClickableIcon::setIcon(const QIcon& newIcon, bool newGrayScale)
+void ClickableIcon::setIcon(const QIcon& newIcon)
 {
   icon = newIcon;
-  grayScale = newGrayScale;
 
   update();
 }
@@ -43,31 +40,9 @@ void ClickableIcon::setEnabled(bool newEnabled)
 void ClickableIcon::paint(QPainter *painter, const QStyleOptionGraphicsItem*, QWidget*)
 {
   QPixmap pixMap = icon.pixmap(defaultSize);
-  if (grayScale)
-  {
-    pixMap = getGrayScale(pixMap);
-  }
-
-  QPixmap grayScale(pixMap.size());
-  grayScale.fill(Qt::transparent);
   painter->setRenderHints(QPainter::SmoothPixmapTransform | QPainter::Antialiasing);
   painter->setOpacity(enabled ? 0.6 : 0.20);
   painter->drawPixmap(0, 0, pixMap);
-}
-
-QPixmap ClickableIcon::getGrayScale(const QPixmap& pixmap) const 
-{
-  QImage im = pixmap.toImage().convertToFormat(QImage::Format_ARGB32);
-  for (int y = 0; y < im.height(); ++y) {
-    QRgb *scanLine = (QRgb*)im.scanLine(y);
-    for (int x = 0; x < im.width(); ++x) {
-      QRgb pixel = *scanLine;
-      uint ci = uint(qGray(pixel));
-      *scanLine = qRgba(ci, ci, ci, qAlpha(pixel));
-      ++scanLine;
-    }
-  }
-  return QPixmap::fromImage(im);
 }
 
 void ClickableIcon::mousePressEvent(QGraphicsSceneMouseEvent*)
--- src/pipeline/view/clickableicon.h
+++ src/pipeline/view/clickableicon.h
@@ -13,9 +13,8 @@ class ClickableIcon : public QObject, public QGraphicsItem
 public:
   ClickableIcon(
     const QIcon& icon,
-    bool grayScale = false,
     QGraphicsItem* parent = nullptr);
-  void setIcon(const QIcon& icon, bool grayScale = false);
+  void setIcon(const QIcon& icon);
   void setEnabled(bool enabled);
   QRectF boundingRect() const override;
   void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
@@ -24,13 +23,11 @@ signals:
   void clicked();
   
 protected:
-  QPixmap getGrayScale(const QPixmap& pixmap) const;
   void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
   void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
   void hoverEnterEvent(QGraphicsSceneHoverEvent*) override;
   void hoverLeaveEvent(QGraphicsSceneHoverEvent*) override;
   QIcon icon;
-  bool grayScale = false;
   bool enabled = true;
 
 private:
